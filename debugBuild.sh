82144818cb9592040f2c610d2fc80d91aa8f7c4d
Zack Ng <ngzhicai98@gmail.com>
2022-12-01 23:49:38 +0800

    build(*): Add mkdir for build folder

--- debugBuild.sh
+++ debugBuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd build
+mkdir -p build && cd build
 
 # init cmake from CMakeLists in project root
 cmake -DCMAKE_BUILD_TYPE=Debug ..
--- releaseBuild.sh
+++ releaseBuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd build
+mkdir -p && cd build
 
 # init cmake from CMakeLists in project root
 cmake -DCMAKE_BUILD_TYPE=Release ..
